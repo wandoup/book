3a682dda52e4310b76b538598d95086d1002a6d5
豌豆炮 <737095020@qq.com>
2020-04-20 09:59:10 +0800

    read

--- pages/read/read.js
+++ pages/read/read.js
@@ -266,15 +266,15 @@ Page({
         }else{
         console.log('访问成功')
         console.log(res)
-        id = res.data.data.data.chapter.id;
-        var content = res.data.data.data.chapter.content;
+        id = res.data.data.chapter.id;
+        var content = res.data.data.chapter.content;
         content = content.replace(/<p>/g, '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
         content = content.replace(/<\/p>/g, '\n');
         content = content.replace(/“/g, '"');
         content = content.replace(/”/g, '"');
           than.setData({
             ['book.name']: aname,
-            ['book.title']: res.data.data.data.chapter.name,
+            ['book.title']: res.data.data.chapter.name,
             ['book.content']: content,
             isShow:"display:block"
           })
@@ -330,67 +330,68 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    // var windowHeight = wx.getSystemInfoSync().windowHeight;
-    // console.log(windowHeight)
-    // var than=this;
-    // num++;
-    // wx.showLoading({
-    //   title: '加载中',
-    //   mask: true
-    // });
-    // wx.getStorage({
-    //   key: 'token',
-    //   success: function (res) {
-    //     header = {
-    //       'content-type': 'application/json',
-    //       'token': res.data
-    //     }
-    // wx.request({
-    //   url: 'https://api.ytool.top/api/content?novel_id=7168&chapter_id=1',
-    //   data: { novel_id: novelidSave, chapter_id: id + num},
-    //   header: header,
-    //   method: 'GET',
-    //   dataType: 'json',
-    //   responseType: 'text',
-    //   success: function (res) {
-    //     if (res.data.code == 0) {
-    //       console.log('获取数据失败')
-    //       num--
-    //     }else{
-    //       console.log(num)
-    //       console.log('访问成功')
-    //       console.log(res)
-    //       var content = res.data.data.data.chapter.content;
-    //       content = content.replace(/<p>/g, '&nbsp;&nbsp;&nbsp;&nbsp;');
-    //       content = content.replace(/<\/p>/g, '\n');
-    //       than.setData({
-    //         ['book.title']: res.data.data.data.chapter.name, 
-    //         // ['book.content']:content,
-    //         ['book.content']: than.data.book.content.concat(content),
-    //         isShow: "display:block"
-    //       })
-    //     }
-    //       // 页面置顶
-    //     // if (wx.pageScrollTo) {
-    //     //   wx.pageScrollTo({
-    //     //     scrollTop: 0
-    //     //   })
-    //     // } else {
-    //     //   wx.showModal({
-    //     //     title: '提示',
-    //     //     content: '当前微信版本过低，请升级到最新微信版本后重试。'
-    //     //   })
-    //     // }
-    //   },
-    //   fail: function (res) {
-    //     console.log('失败')
-    //   },
-    //   complete: function (res) {
-    //     wx.hideLoading();
-    //   },
-    // })
-    //   }
-    // })
+    var windowHeight = wx.getSystemInfoSync().windowHeight;
+    console.log(windowHeight)
+    var than=this;
+    num++;
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    });
+    wx.getStorage({
+      key: 'token',
+      success: function (res) {
+        header = {
+          'content-type': 'application/json',
+          'token': res.data
+        }
+    wx.request({
+      url: 'https://api.ytool.top/api/content?novel_id=7168&chapter_id=1',
+      data: { novel_id: novelidSave, chapter_id: id + num},
+      header: header,
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: function (res) {
+        if (res.data.code == 0) {
+          console.log('获取数据失败')
+          console.log(res)
+          num--
+        }else{
+          console.log(num)
+          console.log('访问成功')
+          console.log(res)
+          var content = res.data.data.chapter.content;
+          content = content.replace(/<p>/g, '&nbsp;&nbsp;&nbsp;&nbsp;');
+          content = content.replace(/<\/p>/g, '\n');
+          than.setData({
+            ['book.title']: res.data.data.chapter.name, 
+            // ['book.content']:content,
+            ['book.content']: than.book.content.concat(content),
+            isShow: "display:block"
+          })
+        }
+          // 页面置顶
+        // if (wx.pageScrollTo) {
+        //   wx.pageScrollTo({
+        //     scrollTop: 0
+        //   })
+        // } else {
+        //   wx.showModal({
+        //     title: '提示',
+        //     content: '当前微信版本过低，请升级到最新微信版本后重试。'
+        //   })
+        // }
+      },
+      fail: function (res) {
+        console.log('失败')
+      },
+      complete: function (res) {
+        wx.hideLoading();
+      },
+    })
+      }
+    })
   },
 
   /**
